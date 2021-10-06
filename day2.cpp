ec9cab18006dedfe925913b847796fb5acbe018f
Parth Maheshwari <74190895+Parth-19@users.noreply.github.com>
2021-10-06 14:28:24 +0530

    v1
    Day_2 - 
    A floor has a center wooden section of size ‘n×m’, that has to be covered with carpet. The carpet has to be stitched using square pieces of cloth of length ‘a’.
    Find the minimum number of cloths that must be stitched together to cover the wooden section. The carpet may cover an additional area outside the wooden floor. The input contains 3 integers, n m a, in the respective order.

---
+++ day2.cpp
@@ -0,0 +1,18 @@
+/* Code365 By Techcider (STME, NMIMS) ; Parth Maheshwari (1st Year MBA Tech CE) */
+
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n,m,a;
+    long long int i,j;
+    cin>>n>>m>>a;
+    i=n/a;
+    j=m/a;
+    if(i*a!=n)
+    i++;
+    if(j*a!=m)
+    j++;
+    cout<<i*j;
+}
