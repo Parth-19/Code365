8b06daa48546cef33b4fcdac28917430cfee2dd7
Parth Maheshwari <74190895+Parth-19@users.noreply.github.com>
2021-10-11 11:40:35 +0530

    v1
    Write a program to print the first ‘n’ layers of the pascal triangle.
    Sample Input - 4
    sample output - 
    1
    1 1 
    1 2 1
    1 3 3 1

---
+++ day5.cpp
@@ -0,0 +1,35 @@
+/* Code365 By Techcider (STME, NMIMS) ;code written by Parth Maheshwari (1st Year MBA Tech CE) */
+/* Pascal's triangle Day 5 in C++*/
+
+#include <iostream>
+using namespace std;
+int binomialCoeff(int n, int k);
+void printPascal(int n)
+{
+	for (int line = 0; line < n; line++)
+	{
+		for (int i = 0; i <= line; i++)
+			cout <<" "<< binomialCoeff(line, i);
+		cout <<"\n";
+	}
+}
+int binomialCoeff(int n, int k)
+{
+	int res = 1;
+	if (k > n - k)
+	k = n - k;
+	for (int i = 0; i < k; ++i)
+	{
+		res *= (n - i);
+		res /= (i + 1);
+	}
+	return res;
+}
+int main()
+{
+	int n = 4;
+	printPascal(n);
+	return 0;
+}
+
+
